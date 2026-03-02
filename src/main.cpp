f69182934f6c9545f6d4211d3a797c83bbd33738
Ivan Yakimov <yoowhi@gmail.com>
2026-03-02 16:11:14 +0300

    trying to run

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,92 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - basic window
-*
-*   Example complexity rating: [★☆☆☆] 1/4
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute 'raylib_compile_execute' script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-*   Web version of the program is generated in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2025 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+#include <raylib.h>
+#include <raymath.h>
+#include <r3d/r3d.h>
 
-#include "raylib.h"
+static R3D_Mesh meshSphere = {};
+static R3D_Material matDefault = {};
+static R3D_InstanceBuffer instanceBuffer = {};
+static Camera3D camDefault = {};
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+int main()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+	// Initialize raylib window
+	const auto window_width = 1280;
+	const auto window_height = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    SetTargetFPS(60);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+	InitWindow(window_width, window_height, "Hello Raylib!");
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+	if (!IsWindowReady()) {
+		TraceLog(LOG_ERROR, "Window failed to initialize");
+		return 1;
+	}
+
+	R3D_Init(window_width, window_height);
+
+	camDefault = (Camera3D) {
+		.position = (Vector3) {0, 2, 2},
+		.target = (Vector3) {0, 0, 0},
+		.up = (Vector3) {0, 1, 0},
+		.fovy = 60,
+	};
+
+    R3D_Light light = R3D_CreateLight(R3D_LIGHT_DIR);
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+        R3D_SetLightDirection(light, (Vector3) { 0, -1, 0 });
+        R3D_SetLightActive(light, true);
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+	const int xInstances = 7;
+	const int yInstances = 7;
 
-    return 0;
+	instanceBuffer = R3D_LoadInstanceBuffer(xInstances*xInstances, R3D_INSTANCE_POSITION);
+	auto* positions = static_cast<Vector3*>(R3D_MapInstances(instanceBuffer, R3D_INSTANCE_POSITION));
+
+	// center the instances
+	const float spacing = 1.0f;
+	const float offsetX = (xInstances - 1) * spacing / 2;
+	const float offsetZ = (yInstances - 1) * spacing / 2;
+
+	for (int x = 0; x < xInstances; x++) {
+		for (int y = 0; y < yInstances; y++) {
+			positions[y * xInstances + x] = { x * spacing - offsetX, 0, y * spacing - offsetZ };
+		}
+	}
+	R3D_UnmapInstances(instanceBuffer, R3D_INSTANCE_POSITION);
+
+    meshSphere = R3D_GenMeshSphere(0.25f, 64, 64);
+	matDefault = R3D_GetDefaultMaterial();
+    {
+        matDefault.albedo.color = (Color){255, 255, 255, 255};
+    }
+
+	R3D_ENVIRONMENT_SET(background.color, (Color){0, 0, 255, 255});
+	R3D_ENVIRONMENT_SET(ambient.color, DARKGRAY);
+
+	while (!WindowShouldClose()) {
+		
+		UpdateCamera(&camDefault, CAMERA_ORBITAL);
+
+		BeginDrawing();
+
+		R3D_Begin(camDefault);
+		R3D_DrawMeshInstanced(meshSphere, matDefault, instanceBuffer, xInstances*yInstances);
+		R3D_End();
+
+		const auto text = "Hello, Raylib and R3D!";
+		const auto text_width = MeasureText(text, 20);
+		const auto text_height = 20;
+
+		DrawText(text, window_width / 2 - text_width / 2, window_height / 2 - text_height / 2, text_height, RED);
+		EndDrawing();
+	}
+	
+	R3D_UnloadInstanceBuffer(instanceBuffer);
+	R3D_UnloadMesh(meshSphere);
+
+	R3D_Close();
+	CloseWindow();
+	return 0;
 }
